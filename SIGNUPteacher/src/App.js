deb2ad928fa121d658fe2a51c4d974ecaa60181b
yeshwanthsnapwiz <yeshwanth.r@snapwiz.com>
2020-04-04 10:44:09 +0530

    sign up page

---
+++ SIGNUPteacher/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from './nav/nav';
+import {PAGEWRAP} from './background';
+import './App.css';
+import {GlobalStyle} from './Appcss'
+import Section from './section/section';
+
+
+function App() {
+  return (
+    <PAGEWRAP>
+    <GlobalStyle></GlobalStyle>  
+    <Nav></Nav>
+    <Section></Section>
+    </PAGEWRAP>
+  );
+}
+
+export default App;
