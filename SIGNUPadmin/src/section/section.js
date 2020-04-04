5f56d215d9c29faba8b7cf7c53fb4ba9d1655fba
yeshwanthsnapwiz <yeshwanth.r@snapwiz.com>
2020-04-04 10:41:22 +0530

    sign up page

---
+++ SIGNUPadmin/src/section/section.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {P,BUTTON,SIGNUP,A,DIV,SIGNIN,Li,PRIVACY,FORM,Label,IMG,SIGNINP,LOCKIMG,CustomInput,InputGroup,Hashtag} from './sectioncss';
+import  Logo from '../Images/google-btn.svg';
+import  Logoa from '../Images/office.svg';
+import  Lock from '../Images/lock.svg';
+import  PREFIXUSER from '../Images/roundeduser.svg';
+import  PREFIXMAil from '../Images/mail.png';
+import { Row, Col, Typography } from 'antd';
+const { Title } = Typography;
+
+
+
+
+
+export const Section = ({className}) => {
+    return (
+
+       <div>
+
+    <Row  justify="space-between" align="middle"  className="ant-layout-content ">
+    <Col align="middle" span={24}  md={12}>
+
+       {/*for students*/}
+
+         <DIV>
+         <Title className="h5" level={5}>for Administrator</Title>
+          <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet,consectetur adipiscing elit.</P>
+
+          <SIGNUP >
+          <A href="#">SIGN UP AS STUDENT  >></A>
+          <A href="#">SIGN UP AS ADMINISTRATOR  >></A>
+          </SIGNUP>
+         </DIV>
+
+    </Col>
+
+    <Col align="middle"  span={24} md={12}>
+
+      <Row justify="space-around" align="middle">
+      <Col align="middle" className="form" flex="454px">
+          
+         {/*sign-in and sign-up block*/}
+
+         <SIGNIN>
+          
+         <Title className="h2" level={2}>Sign up </Title>
+         <Li as="a"><IMG src={Logo}></IMG><SIGNINP>SIGN IN WITH GOOGLE</SIGNINP></Li>
+         <Li as="a"><IMG src={Logoa}></IMG>< SIGNINP>SIGN IN WITH OFFICE 365</SIGNINP></Li>
+         <PRIVACY> <LOCKIMG src={Lock}></LOCKIMG > <p>Edulastic doesn't share your information with third parties <br></br> your data will be completely private and secure.</p></PRIVACY>
+
+         </SIGNIN>
+
+         <FORM>
+
+         <Title className="h2" level={2}>Or create your account </Title>
+    
+        <InputGroup>   <Label htmlFor="name">Name</Label><br></br>
+        <Hashtag src={PREFIXUSER} style={{top:"32px",left:"8px"}}></Hashtag> <CustomInput className="input" align="middle" type="text" placeholder="Insert your name" name="name"  /></InputGroup>
+         <InputGroup>  <Label htmlFor="input">username / email</Label><br></br>
+         <Hashtag src={PREFIXMAil} style={{top:"28px",left:"12px"}}></Hashtag><CustomInput className="input" align="middle" type="email" placeholder="Insert your username or email " name="name" /></InputGroup>
+
+         <InputGroup> <Label htmlFor="classcode">PASSWORD</Label><br></br>
+         <Hashtag src={Lock} style={{top:"30px",left:"10px"}}></Hashtag><CustomInput  className="input" align="middle" type="password" placeholder="Insert your Password" name="classcode"  /></InputGroup>
+         <BUTTON>SIGN UP AS ADMINISTRATOR</BUTTON>
+
+         </FORM>
+
+      </Col>
+      </Row>
+
+    </Col>
+    </Row>
+  </div>
+       
+    )
+}
+
+export default Section;
---
+++ SIGNUPadmin/src/section/sectioncss.js
@@ -0,0 +1,229 @@
+import styled from 'styled-components';
+import {Input ,Button} from 'antd';
+
+export const DIV =styled.div` 
+  display:flex;
+  justify-content:center;
+  width:80%;
+  text-align:left;
+  flex-direction:column;
+  .h5{
+  color:#434B5D;
+  letter-spacing:-3px;
+  line-height:0.8;
+  font-family: 'Open Sans', sans-serif;
+  font-size:60px;
+  font-weight:bold;
+  }
+`;
+
+
+export const P =styled.p` 
+  color:grey;
+  color: #6A737F;
+  font-size:14px;
+  letter-spacing: 0px;
+  line-height:2;
+  color: #6A737F;
+`;
+
+
+export const SIGNUP =styled.div` 
+  display:flex;
+  width:90%;
+  justify-content:space-between;
+  position: relative;
+  top:4px;
+  @media screen and (max-width: 780px) {
+  flex-direction:column;
+  }
+`;
+
+export const IMG=styled.img`
+  width: 17px;
+  height: 20px;
+`;
+
+
+export const A =styled.a` 
+  font-family: 'Open Sans', sans-serif;
+  letter-spacing: 0.2px;
+  color: #1AB395;
+  font-weight:bold;
+  font-size:11px;
+  &:hover{
+  color:#4bb495;
+  text-decoration:underline;
+  }
+`;
+
+export const SIGNIN =styled.div` 
+  justify-content:center;
+  height: 246px;
+  background: #024788 0% 0% no-repeat padding-box;
+  border-radius: 10px 10px 0px 0px;
+  opacity: 1;
+  .h2{
+  position: relative;
+  top:20px;
+  font-family: 'Open Sans', sans-serif;
+  font-weight:bold;
+  font-size:26px;
+  letter-spacing: -1.3px;
+  color: #FFFFFF;
+  opacity: 1;
+}
+`;
+
+export const Li =styled.li` 
+  display:flex;
+  justify-content:space-around;
+  align-items:center;
+  width:90%;
+  height: 40px;
+  list-style-type:none;
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+  border-radius: 4px;
+  opacity: 1;
+  position: relative;
+  top:20px;
+  margin-bottom:8px;
+  .office{
+  width: 17px;
+  height: 20px;
+  background: #E64A19 0% 0% no-repeat padding-box;
+  opacity: 1;
+}
+`;
+
+export const SIGNINP=styled.span`
+  width:80%;
+  position: relative;
+  right:17px;
+  font-family: 'Open Sans', sans-serif;
+  font-weight:bold;
+  font-size:11px;
+  letter-spacing: 0.2px;
+  color: #434B5D;
+  opacity: 1;
+`;
+
+
+export const PRIVACY =styled.li` 
+  display:flex;
+  justify-content:space-around;
+  width:80%;
+  height: 34px;
+  position: relative;
+  top:35px;
+  p{
+  width:80%;
+  text-align: left;
+  font-family: 'Open Sans', sans-serif;
+  font-weight:bold;
+  font-size:10px;
+  letter-spacing: 0px;
+  color: #FFFFFFA3;
+  opacity: 1
+  }
+`;
+
+
+export const LOCKIMG=styled.img`
+
+`;
+
+export const FORM=styled.div`
+  display:flex;
+  flex-direction:column;
+  width:100%;
+  align-self:center;
+  text-align:justify;
+  height:400px;
+  .h2{
+  text-align: center;
+  font-size:16px;
+  font-family: 'Open Sans', sans-serif;
+  letter-spacing: 0.3px;
+  color: #2F4151;
+  position: relative;
+  top:20px;
+}
+`;
+
+export const Label=styled.label`
+  text-transform:uppercase;
+  font-family: 'Open Sans', sans-serif;
+  font-size:12px;
+  letter-spacing: 0px;
+  color: #434B5D;
+  opacity: 1;
+  position: relative;
+  top:15px;
+`;
+
+export const InputGroup=styled.div`
+  width:80%;
+  align-self:center;
+  position: relative;
+  top:25px;
+`;
+
+export const Hashtag=styled.img`
+  position: relative;
+  z-index:200;
+  cursor:pointer;
+`;
+
+export const CustomInput =styled(Input)`
+  height:40px;
+  outline:none;
+  padding-left:35px;
+  font-family: 'Open Sans', sans-serif;
+  background: #F8F8F8 0% 0% no-repeat padding-box;
+  &::placeholder{
+  font-size:12px;
+  font-family: 'Open Sans', sans-serif;
+  letter-spacing: 0.24px;
+  color: #6A737F;
+  }
+  &:hover{
+  border:1px solid #B9B9B9;
+  }
+  &:focus{
+  border:1px solid #1AB395;
+  box-shadow:none;
+  } 
+`;
+
+export const Group =styled.div`
+  width:80%;
+  height:40px;
+  border:1px solid #1AB395;
+  position: relative;
+  top:20px;
+`;
+
+
+export const BUTTON = styled(Button)`
+  width:81%;
+  height:45px;
+  text-transform:uppercase;
+  color:white;
+  background: #1AB395 0% 0% no-repeat padding-box;
+  border-radius: 4px;
+  opacity: 1;
+  position: relative;
+  top:50px;
+  left:1px;
+  align-self:center;
+  font-family: 'Open Sans', sans-serif;
+  &:hover{
+  background: #1AB395 0% 0% no-repeat padding-box;
+  color:white;
+  border:none;
+  }
+`;
+
+
+
